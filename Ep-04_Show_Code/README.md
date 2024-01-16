c8d40db0fd815c31a67e41e6d7652ee9b794a8a1
PRAJAKTA SELUKAR <34501121+PrajaktaSelukar@users.noreply.github.com>
2024-01-16 12:25:43 +0530

    Ep 04 Initial Swiggy design set up

---
+++ Ep-04_Show_Code/README.md
@@ -0,0 +1,5 @@
+# 1. Props (like arguments to a function)
+In React, props (short for properties) are a way to pass data from a parent component to a child component. Props are read-only and cannot be modified by the child component.
+
+# 2. Config driven UI
+User interface (UI) of an application is primarily defined and controlled by configuration rather than hard-coded implementation. This allows for greater flexibility, ease of customization, and dynamic changes to the UI without requiring extensive code modifications.
---
+++ Ep-04_Show_Code/src/components/Body.js
@@ -0,0 +1,19 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockdata";
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                {
+                    resList.map((restaurant, index) => {
+                        return <RestaurantCard key="restaurant.data.id" resData={restaurant} />
+                    })
+                }
+            </div>
+        </div>
+    )
+};
+
+export default Body;
